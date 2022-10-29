3eb68e129a25c2075333648f8e7c774edc336cbf
Mourad Mokane <mmokane@e2r2p4.1337.ma>
2022-10-29 14:42:18 +0100

    m

--- libft.h
+++ libft.h
@@ -48,5 +48,6 @@ int		ft_atoi(const char *str);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 #endif
--- test.c
+++ test.c
@@ -1,37 +1,24 @@
-#include <stdio.h>
-#include "libft.h"
-
-char* ft_strmapi(char const* s, char (*f)(unsigned int, char))
+void	fnc(unsigned int i, char *str)
 {
-	size_t len;
-	size_t i;
-	char* str;
+	i = 0;
+	str[i] -= 32;
+}
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
 
 	i = 0;
-	if (!s || !f)
-		return (0);
-	len = ft_strlen(s);
-	if (!(str = (char*)malloc(sizeof(char) * (len + 1))))
-		return (0);
-	while (i < len)
+	while (s[i])
 	{
-		str[i] = f(i, s[i]);
+		f(i, &s[i]);
 		i++;
 	}
-	str[i] = '\0';
-	return (str);
-}
-char f(unsigned int i, char c)
-{
-	char str;
-	str = c + 1;
-	return (str);
 }
 
 int main()
 {
-	char str1[] = "abc";
-	char *str2;
-	str2 = ft_strmapi(str1, *f);
-	printf("%s\n", str2);
+	char str[] = "hhhhhhhhh";
+	ft_striteri(str, &fnc);
+	printf("%s\n", str);
 }
